91770c97823a34b1a69a4bcbc75aef9a605e5a5f
N4de <jaAnnala@gmail.com>
2019-09-06 16:29:55 +0300

    3.3: puhelinluettelon backend step3

--- index.js
+++ index.js
@@ -38,11 +38,21 @@ app.get('/info', (req, res) => {
   const date = new Date();
   const infos = `Phonebook has info for ${persons.length} people`
   
-
-
   res.send(
     `${infos} </br> </br> ${date}`
   );
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if(person) {
+    res.json(person);
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 const PORT = 3001
