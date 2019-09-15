1e8b2e4d41b40bd47f1f971d7e46f18c1943a145
N4de <jaAnnala@gmail.com>
2019-09-15 18:35:48 +0300

    3.13: puhelinluettelo ja tietokanta, step1

--- index.js
+++ index.js
@@ -13,42 +13,19 @@ app.use(bodyParser.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'));
 morgan.token('person', function (req, res) { return JSON.stringify(req.body) })
 
-let persons = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3
-    },
-    {
-      name: "Mary Poppendieck",
-      number: "39-23-6423122",
-      id: 4
-    }
-];
-
 const generateId = () => {
-  const newId = Math.floor(Math.random() * 1337);
+  const newId = Math.floor(Math.random() * 133337);
 
   return newId;
 }
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/persons', (req, res) => {
 
-  res.json(persons)
+  Contact.find({})
+    .then(contacts => {
+      res.json(contacts.map(contact => contact.toJSON()))
+    });
 })
 
 app.get('/info', (req, res) => {
@@ -62,20 +39,18 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
+  const id = req.params.id;
+  
+  Contact.findById(id).then(contact => {
+    res.json(contact.toJSON())
+  })
 
-  if(person) {
-    res.json(person);
-  }
-  else {
-    res.status(404).end();
-  }
+  
 });
 
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
-  persons = persons.filter(note => note.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   res.status(204).end()
 })
--- models/contact.js
+++ models/contact.js
@@ -4,7 +4,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => {
     console.log('connected to MongoDB')
   })
@@ -17,7 +17,7 @@ const contactSchema = new mongoose.Schema({
     number: String,
 })
 
-noteSchema.set('toJSON', {
+contactSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
